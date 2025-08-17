b77f5443bcc6a670a97969c0a735b4c45641e3c2
deev-narayan <svnarayan333@gmail.com>
2025-08-17 10:41:47 +0530

    some updation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:attendo/styles/app_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,9 +17,32 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: Scaffold(
-        appBar: AppBar(title: Text("BBAU Attendo")),
-        body: Scaffold(),
+        body: Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(color: const Color.fromARGB(94, 71, 71, 71)),
+            ),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(40, 150, 40, 150),
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(92, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: const Color.fromARGB(90, 0, 0, 0))
+                )
+              ),
+            )
+            
+          ],
+        ),
       ),
     );
   }
