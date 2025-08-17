a17f731743cda5faa05dad51067c009fbaf5b567
himanshu yadav <himanshuyadav1740343@gmail.com>
2025-08-17 11:28:33 +0530

    fixed smth

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,5 @@
 import 'package:attendo/pages/profile.dart';
 import 'package:flutter/material.dart';
-import 'package:attendo/styles/app_color.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -13,51 +12,47 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(94, 71, 71, 71),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
-                  height: MediaQuery.of(context).size.height,
+                  margin: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+                  height: 450,
                   width: double.infinity,
                   decoration: BoxDecoration(
-                    color: const Color.fromARGB(94, 71, 71, 71),
+                    color: const Color.fromARGB(92, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: const Color.fromARGB(90, 0, 0, 0),
+                    ),
                   ),
                 ),
-                Center(
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(40, 150, 40, 150),
-                    width: double.infinity,
-                    constraints: BoxConstraints(
-                      minHeight: 200,
-                      maxHeight: MediaQuery.of(context).size.height - 300,
-                    ),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(92, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: const Color.fromARGB(90, 0, 0, 0),
+                FilledButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return Profile();
+                        },
                       ),
-                    ),
-                  ),
+                    );
+                  },
+                  child: Text("login"),
                 ),
               ],
             ),
-            FilledButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Profile();
-                    },
-                  ),
-                );
-              },
-              child: Text("Login"),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
